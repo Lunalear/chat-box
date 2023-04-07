2112ea911675a1846d64b0e9e6820d2336d27588
Lunalear <115168891+Lunalear@users.noreply.github.com>
2023-04-07 15:59:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 });
 
-const server = app.listen(4000,()=>{
+const server = app.listen(process.env.PORT||4000,()=>{
     console.log('Server on port 4000');
 });
 
